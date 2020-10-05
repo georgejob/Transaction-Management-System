a1a29eb32977453c739af548ab0015b1ed9820ef
georgejob <45026762+georgejob@users.noreply.github.com>
2020-10-05 17:50:04 -0400

    Add files via upload

--- Profile.java
+++ Profile.java
@@ -2,4 +2,20 @@
 public class Profile {
 	private String fname;
 	private String lname;
+	
+	public Profile(String fname, String lname)
+	{
+		this.fname = fname;
+		this.lname = lname;
+	}
+	
+	public String getFname()
+	{
+		return fname;
+	}
+	
+	public String getLname()
+	{
+		return lname;
+	}
 }
